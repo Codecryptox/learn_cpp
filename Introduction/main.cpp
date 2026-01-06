5e26ffd4312d49d22b6d1e27c33a02fb4bfa412f
Akash Kolay <akashkolay43@gmail.com>
2026-01-07 00:03:44 +0530

    Introduction

---
+++ Introduction/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+// to run a cpp program in you have to use the following command :
+// g++ your_program.cpp -o your_program 
+// your_program
+//(Here g++ is the compiler for cpp and your program.cpp is input file)
+// -o stands for output exceutable file then the file name you can name it anything 
+// type the name of excetutable to run it on windows
+
+int main(){
+    std::cout << "It prints some text" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
